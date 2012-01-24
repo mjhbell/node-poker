3acd98e185e22d534a1e74f4e65656bf48dd79d6
Michael Bell <mike@mjhbell.com>
2012-01-24 13:32:59 +0000

    Updated to reflect code changes.

--- example.js
+++ example.js
@@ -1,12 +1,9 @@
 var poker = require('node-poker');
 
-var table = new poker.Table(10,20,2,10,'table_1',100,1000);
+var table = new poker.Table(50,100,2,10,'table_1',100,1000);
 
 table.AddPlayer('bob',1000)
 table.AddPlayer('jane',500)
-
-table.players[0].Bet(10);
-table.players[1].Bet(20);
 
 table.players[0].Call();
 
