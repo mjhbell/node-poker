f7063bcebb931a8696886a0e47c252e76d76e96c
Michael Bell <mike@mjhbell.com>
2012-01-24 09:09:56 +0000

    Fixed typo, missing closing bracket.

--- example.js
+++ example.js
@@ -1,6 +1,6 @@
 var poker = require('node-poker');
 
-var table = new poker.Table(10,20,2,10,'table_1',100,1000;
+var table = new poker.Table(10,20,2,10,'table_1',100,1000);
 
 table.AddPlayer('bob',1000)
 table.AddPlayer('jane',500)
