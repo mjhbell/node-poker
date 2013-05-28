31267631cbb05060a05f373286c9167b0cbdf441
Gauthier Boivin <g.boivin@novediagroup.com>
2013-05-28 17:58:58 +0200

    Add multiple round and Fix player and game functions

--- example.js
+++ example.js
@@ -1,10 +1,31 @@
-var poker = require('node-poker');
+var poker = require('./node-poker');
 
 var table = new poker.Table(50,100,2,10,'table_1',100,1000);
 
 table.AddPlayer('bob',1000)
-table.AddPlayer('jane',500)
+table.AddPlayer('jane',1000)
+table.AddPlayer('dylan',1000)
+table.AddPlayer('john',1000)
 
+table.StartGame()
+
+table.players[1].Call();
+table.players[2].Call();
+table.players[3].Call();
 table.players[0].Call();
+table.players[1].Call();
+table.players[2].Call();
+table.players[3].Call();
+table.players[0].Call();
+table.players[1].Call();
+table.players[2].Call();
+table.players[3].Call();
+table.players[0].Call();
+table.players[1].Call();
+table.players[2].Call();
+table.players[3].Call();
+table.players[0].Call();
+
+table.initNewRound()
 
 console.log(table);
