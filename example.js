5694cb5d53e28058c0717875d70b835013774eb7
Gauthier Boivin <g.boivin@novediagroup.com>
2013-06-03 17:46:39 +0200

    Clean code with jsLint review

--- example.js
+++ example.js
@@ -1,6 +1,6 @@
 var poker = require('./node-poker');
 
-var table = new poker.Table(50,100,2,10,'table_1',100,1000);
+var table = new poker.Table(50,100,2,10,100,1000);
 
 table.AddPlayer('bob',1000)
 table.AddPlayer('jane',1000)
